25e762e6be20f0c059eb1a6987398a49c52c8dbc
Kaan Kafali <99081499@mydavinci.nl>
2025-01-30 12:13:10 +0100

    Update deel3_2.py

--- deel3_2.py
+++ deel3_2.py
@@ -10,20 +10,35 @@ for file in dir:
         data = json.load(f)
         orders.append((file, data))
 
-for order in orders:
+for order_index, order in enumerate(orders):
 
-    producten = ""
+    producten = []
     prijs = 0
     prijs_met_btw = 0
     aantal = 0
     productomschrijvingen = []
+    
+    aantal_text = ["AANTAL", ""]
+
+    btw_text = ["BTW", ""]
+
+    excl_btw_text = ["EXCL. BTW", ""]
+
+    begin_text = ["Tunari",
+    "Romboutslaan 34",
+    "3312 KP, Dordrecht","",
+    "Kvk: " + order[1]["factuur"]["klant"]["KVK-nummer"],
+    "Btw: NL123456789B01"]
+
     for index, product in enumerate(order[1]["factuur"]["producten"]):
-        producten += product["productnaam"]
-        if index != len(order[1]["factuur"]["producten"]) - 1: producten += ", "
+        producten.append(product["productnaam"])
         prijs += product["aantal"] * product["prijs_per_stuk_excl_btw"]
         prijs_met_btw += product["aantal"] * product["prijs_per_stuk_excl_btw"] * (1 + product["btw_per_stuk"] / 100)
         aantal += product["aantal"]
         productomschrijvingen.append(product["productnaam"] + "     PRIJS: " + str(product["aantal"]) + "x " + str(round(product["prijs_per_stuk_excl_btw"], 2)) + "     BTW: " + str(product["btw_per_stuk"]) + "%     TOTAAL: " + str(round(product["aantal"] * product["prijs_per_stuk_excl_btw"], 2)) )
+        aantal_text.append(str(product["aantal"]))
+        btw_text.append(str(product["btw_per_stuk"]) + "%")
+        excl_btw_text.append("€" + str(round(product["prijs_per_stuk_excl_btw"], 2)))
     
     factuurdatum = order[1]["factuur"]["factuurdatum"]
     betaaltermijn = int(order[1]["factuur"]["betaaltermijn"][:-6])
@@ -39,11 +54,7 @@ for order in orders:
                 maand = 1
                 jaar += 1
 
-    text = ["Tunari",
-    "Romboutslaan 34",
-    "3312 KP, Dordrecht","",
-    "Kvk: " + order[1]["factuur"]["klant"]["KVK-nummer"],
-    "Btw: NL123456789B01", "",
+    text = [
     "Aan:",
     order[1]["factuur"]["klant"]["naam"],
     order[1]["factuur"]["klant"]["adres"],
@@ -52,12 +63,12 @@ for order in orders:
     "Factuurnummer: " + order[1]["factuur"]["factuurnummer"],
     "Factuurdatum: " + order[1]["factuur"]["factuurdatum"],
     "Einddatum betaling:", f"{dag:02d}-{maand:02d}-{jaar}", "",
-    "Omschrijving Aantal Bedrag BTW Totaal",
-    "Producten: ", "  " + producten, ""
+    "Producten: ",
+    "-------------------------------------------------------------------------------------------------------------------------------"
     ]
 
-    for productomschrijving in productomschrijvingen:
-        text.append(productomschrijving)
+    for product in producten:
+        text.append(product)
     
     text += ["",
     "TOTAAL EXCL. BTW",
@@ -71,5 +82,15 @@ for order in orders:
     c.setFont("Helvetica", 9)
     for index, line in enumerate(text):
         c.drawString(20, 750 - (index * 13), line)
-    #c.drawImage("logo.png", 2, 790, 150, 50, mask=[0, 0, 0])
+    for index, line in enumerate(begin_text):
+        c.drawString(400, 800 - (index * 13), line)
+    for index, line in enumerate(aantal_text):
+        c.drawString(180, 607 - (index * 13), line)
+    for index, line in enumerate(btw_text):
+        c.drawString(250, 607 - (index * 13), line)
+    for index, line in enumerate(excl_btw_text):
+        c.drawString(300, 607 - (index * 13), line)
+    c.drawImage("logo.png", 2, 790, 150, 50, mask=[0, 0, 0])
     c.save()
+
+    print(f"Order {order[0]} is verwerkt. {order_index}")
